465d1a6b02843b7c577b96ae16bd2dbd9ff56a98
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-01-19 13:43:55 +0200

    Enable also TokenAuthentication when debug is enabled

--- backend/datahubhel/settings.py
+++ backend/datahubhel/settings.py
@@ -127,6 +127,7 @@ REST_FRAMEWORK = {
     ] + ([
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ] if DEBUG else []),
 }
 
