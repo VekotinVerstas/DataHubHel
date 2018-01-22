52914cca44d9751f602c8e4f0b9c1a59de77c069
Frank Wickström <frank.wickstrom@anders.fi>
2018-01-22 09:38:54 +0200

    Allow browsable API only in debug mode

--- backend/datahubhel/settings.py
+++ backend/datahubhel/settings.py
@@ -122,6 +122,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ] + ([
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ] if DEBUG else []),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ] + ([
