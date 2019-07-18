44662379ff977629d13dd44908fb0e8197185286
Deepak Panta <deepak.panta@anders.fi>
2019-07-18 11:25:08 +0300

    Add required models to the application
    Add Location, Sensor, Thing and ThingLocation to models.
    ThingLocation is the through model to connect the Things and the
    Locations.
    
    Refs None

---
+++ backend/ta120_adapter/models.py
@@ -0,0 +1,48 @@
+from django.contrib.gis.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from utils.models import TimestampedUUIDModel
+
+
+class Location(models.Model):
+    name = models.CharField(verbose_name=_("address"), max_length=100)
+    description = models.TextField(blank=True)
+    location = models.PointField(
+        verbose_name=_("location"),
+        null=True,
+        blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Thing(TimestampedUUIDModel):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ThingLocation(TimestampedUUIDModel):
+    location = models.ForeignKey(
+        Location,
+        related_name='thinglocations',
+        on_delete=models.PROTECT)
+    thing = models.ForeignKey(
+        Thing,
+        related_name='thinglocations',
+        on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}--{}".format(self.location.name, self.thing.name)
+
+
+class Sensor(TimestampedUUIDModel):
+    name = models.CharField(max_length=60)
+    sensor_type = models.CharField(max_length=60)
+    thing = models.ForeignKey(Thing, on_delete=models.PROTECT)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
