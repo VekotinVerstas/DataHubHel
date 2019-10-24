c0cd27af061b03c41ea88b7afbf5b90f3e071070
Deepak Panta <deepak.panta@anders.fi>
2019-10-24 16:42:03 +0300

    Rename Sensor in ta120_adapter to TA120Sensor
    Rename Sensor in ta120_adapter application to TA120Sensor and
    inherit from the core Sensor.

--- backend/ta120_adapter/models.py
+++ backend/ta120_adapter/models.py
@@ -2,6 +2,7 @@ from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from datahubhel.base_models import TimestampedUUIDModel
+from datahubhel.core.models import Sensor
 
 
 class Location(models.Model):
@@ -38,14 +39,5 @@ class ThingLocation(TimestampedUUIDModel):
         return "{}--{}".format(self.location.name, self.thing.name)
 
 
-class Sensor(TimestampedUUIDModel):
-    sensor_id = models.CharField(max_length=60, unique=True)
+class TA120Sensor(Sensor):
     key = models.CharField(max_length=128)
-
-    name = models.CharField(max_length=60)
-    sensor_type = models.CharField(max_length=60)
-    thing = models.ForeignKey(Thing, on_delete=models.PROTECT)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
